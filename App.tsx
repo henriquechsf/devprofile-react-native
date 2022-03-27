be7f8a1f360817db2ec8c1359856136c46492185
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-03-27 20:02:12 -0300

    install font family on theme

--- App.tsx
+++ App.tsx
@@ -2,8 +2,23 @@ import React, { FC } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Home } from './src/screens/Home';
 import theme from './src/global/styles/theme';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
 
 const App: FC = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Home />
