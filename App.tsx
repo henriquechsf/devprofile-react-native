98cca0508185c3eadd1e2550782034e90693d385
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-03-27 19:25:51 -0300

    add global theme from app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { FC } from 'react';
+import { ThemeProvider } from 'styled-components';
 import { Home } from './src/screens/Home';
+import theme from './src/global/styles/theme';
 
 const App: FC = () => {
-  return <Home />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 };
 
 export default App;
