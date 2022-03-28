e49ec662d8784f7e17c8a9b9e63bce9139fabe16
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-03-27 21:04:17 -0300

    feat: implement Header component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  Icon,
+  UserAvatar,
+  UserAvatarButton,
+  UserGreeting,
+  UserInfo,
+  UserInfoDetail,
+  UserName,
+  UserWrapper,
+} from './styles';
+
+import avatarDefault from '../../assets/avatar02.png';
 
 export const Home = () => {
   return (
     <Container>
-      <Title>Home Page</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <UserAvatarButton onPress={() => {}}>
+              <UserAvatar source={avatarDefault} />
+            </UserAvatarButton>
+            <UserInfoDetail>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Henrique</UserName>
+            </UserInfoDetail>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 };
