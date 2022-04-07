ca1c80d94929d35cc5c09dc484839888e6f4e7c1
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-04-07 17:35:17 -0300

    feat: async storage to save user authenticated

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Alert } from 'react-native';
 import { api } from '../services/api';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { IUser } from '../model/user';
+
+interface IAuthState {
+  token: string;
+  user: IUser;
+}
 
 interface ICredentials {
   email: string;
@@ -8,7 +15,7 @@ interface ICredentials {
 }
 
 interface IAuthContext {
-  name: string;
+  user: IUser;
   signIn(credentials: ICredentials): void;
 }
 
@@ -20,11 +27,35 @@ export const AuthContext = React.createContext<IAuthContext>(
   {} as IAuthContext,
 );
 
+const tokenData = '@DevProfile:token';
+const userData = '@DevProfile:user';
+
 export const AuthProvider: React.FC<IProps> = ({ children }) => {
+  const [data, setData] = React.useState<IAuthState>({} as IAuthState);
+
+  React.useEffect(() => {
+    async function loadAuthData() {
+      const token = await AsyncStorage.getItem(tokenData);
+      const user = await AsyncStorage.getItem(userData);
+
+      if (token && user) {
+        setData({ token, user: JSON.parse(user) });
+      }
+    }
+
+    loadAuthData();
+  }, []);
+
   const signIn = async ({ email, password }: ICredentials) => {
     try {
       const response = await api.post('sessions', { email, password });
-      console.log(response);
+
+      const { token, user } = response.data;
+
+      await AsyncStorage.setItem(tokenData, token);
+      await AsyncStorage.setItem(userData, JSON.stringify(user));
+
+      setData({ token, user });
     } catch (error) {
       // throw new Error(error as string);
       Alert.alert(
@@ -35,7 +66,7 @@ export const AuthProvider: React.FC<IProps> = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ name: 'Name Provider', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
